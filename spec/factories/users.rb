30e0a794872a6a26aade31b75d98f1d66fbed0c7
Chris Lerum <chris.lerum@gmail.com>
2015-02-02 11:29:07 -0500

    x

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
     password "password"
+    factory :admin_user do
+      admin true
+    end
   end
-
 end
